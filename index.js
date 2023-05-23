1be5d4b0c2875e9361c214536e7eceeff57c1648
Tng_Dng <t.dng08@gmail.com>
2023-05-23 17:05:12 +0700

    test

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+'use strict'
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
 
+app.use(bodyParser.urlencoded(
+    { extended: false}
+));
+
+app.use(bodyParser.json());
+
 app.get('/',(req,res) => {
-    res.send('Home Page')
+    res.send('Hello World')
 });
+
+app.get('/webhook',(req,res) => {
+
+})
 
 app.listen(process.env.PORT,() => {
     console.log(`Server is running on port ${process.env.PORT}`);
