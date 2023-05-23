8bb84f6a0efc0d90fe7125d47eb26cfe04e0d153
Tng_Dng <t.dng08@gmail.com>
2023-05-23 18:14:20 +0700

    pic

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 'use strict'
 const express = require('express')
 const bodyParser = require('body-parser');
+const ejs = require('ejs')
 
 const app = express()
 
+//bodyparser
 app.use(bodyParser.urlencoded(
     { extended: false}
 ));
-
 app.use(bodyParser.json());
 
+//using template engine
+app.set('views');
+app.set('view engine','ejs');
+
+//static file
+app.use('/public',express.static(__dirname+'/public'));
+
+
+//get home
 app.get('/',(req,res) => {
-    res.send('Hello World')
+    res.render('index');
 });
 
+//get webhook
 app.get('/webhook',(req,res) => {
 
 })
 
+//running app
 app.listen(process.env.PORT,() => {
     console.log(`Server is running on port ${process.env.PORT}`);
 })
