ad1fa92fccce4405b46af7683c2e860a02a390a1
Tng_Dng <t.dng08@gmail.com>
2023-05-23 16:51:02 +0700

    port

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
 
 app.get('/',(req,res) => {
     res.send('Home Page')
 });
 
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT,() => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
