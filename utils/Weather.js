11c39dcc9da5186f6c9670c0e7999c6e4310598c
Tng_Dng <t.dng08@gmail.com>
2023-06-01 01:24:26 +0700

    fix res

--- utils/Weather.js
+++ utils/Weather.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 const getWeather = async() => {
     const apiURL = process.env.WEATHER_API_URL;
 
-    const data = await axios.get(apiURL);
+    const res = await axios.get(apiURL);
 
-    const temp = data.main.temp + '℃'
-    const tempFeellike = data.main.feels_like + '℃'
-    const humidity = data.main.humidity + '%'
-    const visibility = data.visibility + 'km'
+    const temp = res.data.main.temp + '℃'
+    const tempFeellike = res.data.main.feels_like + '℃'
+    const humidity = res.data.main.humidity + '%'
+    const visibility = res.data.visibility + 'km'
 
 
     const weatherStr = `Bây giờ nhiệt độ Hà Nội là ${temp} (cảm giác như ${tempFeellike}).Độ ẩm hiện tại là ${humidity},Tầm nhìn hiện tại là ${visibility}`;
